99447ebf20bc257b260dd205fc8a78a08b8c8104
Chan Kang <chankang17@gmail.com>
2024-11-20 22:00:45 -0500

    ignore not found + namespace on helm uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,16 @@
 set -eux
 # https://docs.github.com/en/actions/hosting-your-own-runners/managing-self-hosted-runners-with-actions-runner-controller/deploying-runner-scale-sets-with-actions-runner-controller#upgrading-arc
-helm uninstall arc-runner-set --wait
-helm uninstall large-arc-runner-set --wait
-helm uninstall arc
+helm uninstall arc-runner-set \
+    --wait \
+    --namespace arc-runners \
+    --ignore-not-found
+helm uninstall large-arc-runner-set \
+    --wait \
+    --namespace large-arc-runners \
+    --ignore-not-found
+helm uninstall arc \
+    --wait
+    --namespace arc-systems \
+    --ignore-not-found
 
 kubectl get crds -oname | grep "actions.github.com" | xargs kubectl delete
