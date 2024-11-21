ea78531692495572cf3c0a9b35d4159d84028728
Chan Kang <chankang17@gmail.com>
2024-11-20 21:57:38 -0500

    flesh out the rest of uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,7 @@
 set -eux
-
+# https://docs.github.com/en/actions/hosting-your-own-runners/managing-self-hosted-runners-with-actions-runner-controller/deploying-runner-scale-sets-with-actions-runner-controller#upgrading-arc
 helm uninstall arc-runner-set --wait
 helm uninstall large-arc-runner-set --wait
 helm uninstall arc
+
+kubectl get crds -oname | grep "actions.github.com" | xargs kubectl delete
