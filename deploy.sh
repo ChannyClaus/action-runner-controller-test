adb8aa325a98b7a598f6b4cc9e5ed7f15520f63b
Chan Kang <chankang17@gmail.com>
2024-11-20 20:40:39 -0500

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+set -eux
+
+GITHUB_CONFIG_URL=https://github.com/ChannyClaus/action-runner-controller-test
+
+helm install arc \
+    --namespace "arc-systems" \
+    --create-namespace \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
+
+helm install "arc-runner-set" \
+    --namespace "arc-runners" \
+    --create-namespace \
+    --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
+    --set githubConfigSecret.github_token="${GITHUB_PAT}" \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
+
+helm install "large-arc-runner-set" \
+    --namespace "large-arc-runners" \
+    --create-namespace \
+    --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
+    --set githubConfigSecret.github_token="${GITHUB_PAT}" \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
